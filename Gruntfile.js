3751e67f6d3cdec852a92baa8e22e0486d0dcd14
Craig <cmcnamara87@gmail.com>
2014-11-10 14:51:51 +1000

    refactor(build): change font awesome location

--- Gruntfile.js
+++ Gruntfile.js
@@ -466,7 +466,7 @@ module.exports = function(grunt) {
                     dest: '<%= yeoman.dist %>'
                 }, {
                     expand: true,
-                    cwd: 'bower_components/font-awesome/fonts',
+                    cwd: 'bower_components/fontawesome/fonts',
                     src: ['**'],
                     dest: '<%= yeoman.dist %>/fonts'
                 }, {
